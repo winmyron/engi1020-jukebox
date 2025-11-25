6aa5ed0494e3cf672a1a673094ce6b5df3d2cede
Winter Myron <wintermyron@mun.ca>
2025-11-25 11:05:03 -0330

    Create constants file

---
+++ consts.py
@@ -0,0 +1,12 @@
+BUZZER_PIN = 5
+BUTTON_PIN = 6
+JOYSTICK_PIN = 0
+
+SONG_LIST = [{
+        "name": "Mary Had A Little Lamb",
+        "file": "songs/mary-had-a-little-lamb.csv"
+    },
+    {
+        "name": "Yankee Doodle",
+        "file": "songs/yankee-doodle.csv"
+    }]

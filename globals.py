7c757a7be51f656af97d6ef54f78c268cd61d586
Winter Myron <wintermyron@mun.ca>
2025-12-02 00:02:45 -0330

    FINAL. COMMIT.

---
+++ globals.py
@@ -0,0 +1,31 @@
+# Analog pins
+ROTARY_PIN: int = 0
+
+# Digital pins
+BUZZER_PIN: int = 5
+BUTTON_PIN: int = 6
+
+
+SONG_LIST: list[dict[str, str]] = [
+    {
+        "name": "Mary Had A Little Lamb",
+        "file": "songs/mary-had-a-little-lamb.csv"
+    },
+    {
+        "name": "Yankee Doodle",
+        "file": "songs/yankee-doodle.csv"
+    },
+    {
+        "name": "Wheels on the Bus",
+        "file": "songs/wheels-on-the-bus.csv"
+    }
+]
+
+# Index of the song currently being played in SONG_LIST
+g_current_song_index: int = -1
+
+def function_to_make_this_module_import():
+    """
+    This function does nothing. It is only in here to make my editor shut up about not being able to resolve the import.
+    """
+    pass
